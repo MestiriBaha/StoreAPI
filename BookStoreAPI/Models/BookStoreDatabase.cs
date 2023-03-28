c0adf561d83a2132f18fd615ebb9317d1ed7f5cc
baha mestiri <mohamedbaha.mestiri@esprit.tn>
2023-03-28 23:56:29 +0100

    Configuring the Database settings

---
+++ BookStoreAPI/Models/BookStoreDatabase.cs
@@ -0,0 +1,9 @@
+﻿namespace BookStoreAPI.Models
+{
+    public class BookStoreDatabase
+    {
+        public String? ConnectionString { get; set; }
+        public String? DatabaseName { get; set; }
+        public String BooksCollectionName { get; set; } = null!;
+    }
+}
